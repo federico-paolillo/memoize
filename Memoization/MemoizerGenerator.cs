ac27d4beaa0229e958fab717b41e3bff5ed2db38
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2022-03-01 17:15:03 +0100

    Completed generation of static Memoizer helpers

--- Memoization/MemoizerGenerator.cs
+++ Memoization/MemoizerGenerator.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
@@ -7,23 +8,54 @@ namespace Memoization;
 [Generator]
 public sealed class MemoizerGenerator : ISourceGenerator
 {
+    private const int MAX_GENERIC_PARAMETERS = 16;
+
     public void Initialize(GeneratorInitializationContext context)
     {
     }
 
     public void Execute(GeneratorExecutionContext context)
     {
+        GenerateMemoizerClasses(context);
+        GenerateStaticMemoizerOverloads(context);
+    }
+
+    private static void GenerateMemoizerClasses(GeneratorExecutionContext context)
+    {
+        
+    }
+    
+    private static void GenerateStaticMemoizerOverloads(GeneratorExecutionContext context)
+    {
+        for (var numberOfParameters = 1; numberOfParameters < MAX_GENERIC_PARAMETERS + 1; numberOfParameters++)
+        {
+            GenerateMemoizerOverload(context, numberOfParameters);
+        }
+    }
+
+    private static void GenerateMemoizerOverload(GeneratorExecutionContext context, int numberOfParameters)
+    {
+        var genericParametersList =
+            string.Join(", ", Enumerable.Range(1, numberOfParameters).Select(pIndex => $"T{pIndex:D}"));
+
         var sourceSb = new StringBuilder();
 
         sourceSb.AppendLine("using System;");
-        
+
         sourceSb.AppendLine("namespace Memoization;");
+
+        sourceSb.AppendLine(@$"public static partial class Memoizer {{
         
-        sourceSb.AppendLine(@"public static class Memoizer {
-            public static void Memoize<T1, TOut>(Func<T1, TOut> fn) {
-            }
-        }");
-        
-        context.AddSource("memoizer.g.cs", SourceText.From(sourceSb.ToString(), Encoding.UTF8));
+            public static void Memoize<{genericParametersList}, TOut>(Func<{genericParametersList}, TOut> fn) {{
+
+                
+
+            }}    
+            
+        }}");
+
+        var sourceText = SourceText.From(sourceSb.ToString(), Encoding.UTF8);
+
+        context.AddSource($"Memoizer.{numberOfParameters:D2}.g.cs", sourceText);
     }
 }
